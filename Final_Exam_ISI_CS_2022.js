839c988bd19f6048bb3eff20b3a962645b9e3ea2
Arwaa Ahmed <arwaa.ahmed@islamicschoolofirving.org>
2022-05-15 22:38:15 -0500

    first commit

---
+++ Final_Exam_ISI_CS_2022.js
@@ -0,0 +1,6 @@
+const pic = document.createElement("div");
+pic.setAttribute("id", "new-div");
+pic.innerText = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio itaque ex modi inventore velit non quas quia officia! Ad tenetur inventore labore quam nulla officiis ipsum optio aliquid nihil laborum."
+document.body.appendChild(pic);
+
+document.getElementById("my-final").innerHTML = "Hello ISI, I am done!";
